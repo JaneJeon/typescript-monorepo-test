6d7c54102f700dfba90c6d73330d73a61b7fa42a
Jane Jeon <git@janejeon.com>
2023-06-26 17:57:32 +0900

    use in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
+import { useGetUsers } from 'client'
+
+axios.defaults.baseURL = 'http://localhost:3000'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const { data, isLoading } = useGetUsers()
 
   return (
     <>
@@ -28,6 +34,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      {isLoading ? (
+        <p>Loading users...</p>
+      ) : (
+        <p>Loaded users: {JSON.stringify(data, null, 2)}</p>
+      )}
     </>
   )
 }
