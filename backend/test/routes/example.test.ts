e7869b4774b59ce35fca5d407f77aa4bb7b331e0
Jane Jeon <git@janejeon.com>
2023-06-26 17:51:42 +0900

    prettier --write .

--- backend/test/routes/example.test.ts
+++ backend/test/routes/example.test.ts
@@ -1,7 +1,7 @@
 import { test } from 'tap'
 import { build } from '../helper'
 
-test('example is loaded', async (t) => {
+test('example is loaded', async t => {
   const app = await build(t)
 
   const res = await app.inject({
